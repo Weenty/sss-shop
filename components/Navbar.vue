290d91f56b1036f3beb3cdfd1dfb835e152a59e7
Weenty <flaky12r@mail.ru>
2021-12-18 10:59:12 +0500

    tt

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -9,9 +9,68 @@
       </div>
       <div class="col"><div class="button-profile">Profile</div></div>
       <div class="col">
-        <button @click="visible = !visible" class="button-backet">
+        <button
+          @click="[$modal.show('my-modal')]"
+          class="button-backet"
+        >
           Basket <a v-if="basket !== 0">({{ basket }}) </a>
         </button>
+        <!-- <modal
+          name="my-modal"
+          :width="455"
+          :height="790"
+          :shiftX="1"
+          :shiftY="0"
+          :adaptive="true"
+        >
+          <div class="modal-content">
+            <div class="container">
+              <div style="padding-top: 50px" class="row">
+                <div class="col">
+                  <div style="text-align: center" class="button-profile">
+                    Profile
+                  </div>
+                </div>
+                <div class="col">
+                  <button style="text-align: center" class="button-backet">
+                    Basket <a v-if="basket !== 0">({{ basket }}) </a>
+                  </button>
+                </div>
+              </div>
+
+              <div class="line" />
+              <div class="row">
+                <div
+                  style="
+                    font-family: Inter;
+                    font-style: normal;
+                    font-weight: bold;
+                    font-size: 24px;
+                    line-height: 29px;
+                    padding-top: 20px
+                    text-transform: uppercase;
+                    color: #000000;
+                  "
+                  class="col-12"
+                >
+                  TOTAL
+                </div>
+                <div class="row" id="modal-row" >
+                  <div class="col-6">Subtotal: </div>
+                  <div class="col-6">$99,99</div>
+                </div>
+                <div class="row" id="modal-row">
+                  <div class="col-6">Sale: </div>
+                  <div class="col-6">$34:00</div>
+                </div>
+                <div class="row" id="modal-row">
+                  <div class="col-6">Content</div>
+                  <div class="col-6">Remove all</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </modal> -->
       </div>
     </div>
     <div class="line" />
@@ -22,7 +81,6 @@ export default {
   data() {
     return {
       basket: 2,
-      visible: false,
     };
   },
 };
@@ -59,6 +117,11 @@ export default {
   font-size: 18px;
   line-height: 22px;
   color: #000000;
+}
+#modal-row {
+  width:100%; 
+  text-align: center;
+  margin-top: 20px;
 }
 .button-backet {
   outline: none;
